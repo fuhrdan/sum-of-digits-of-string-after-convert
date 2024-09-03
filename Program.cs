d146cbb21d2eb05956252ff70790f7c12d2a9487
fuhrdan <165685619+fuhrdan@users.noreply.github.com>
2024-09-03 07:04:58 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿//*****************************************************************************
+//** 1945 Sum of Digits of String After Convert  leetcode                    **
+//*****************************************************************************
+
+
+int getLucky(char* s, int k) {
+    int len = strlen(s);
+    int* convert = (int*)malloc(len * sizeof(int));
+    int totesMyGoats = 0;
+    // Convert each character to a number and then sum their digits
+    for (int i = 0; i < len; i++) {
+        int num = s[i] - 'a' + 1; // Convert character to number ('a' -> 1, 'b' -> 2, etc.)
+        // Sum digits of the number (e.g., 'j' -> 10 -> 1 + 0 = 1)
+        while (num > 0) {
+            totesMyGoats += num % 10;
+            num /= 10;
+        }
+    }
+
+    // Repeat the summing process k-1 more times
+    for (int j = 1; j < k; j++) {
+        int newSum = 0;
+        while (totesMyGoats > 0) {
+            newSum += totesMyGoats % 10;
+            totesMyGoats /= 10;
+        }
+        totesMyGoats = newSum;
+    }
+    free(convert);
+    return totesMyGoats;
+}
